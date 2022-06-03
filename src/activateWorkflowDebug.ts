8bf7e8e3e4b4ca5e1ff0f160eca226a9d001a4dc
artificial-ben <88675060+artificial-ben@users.noreply.github.com>
2022-06-03 17:40:34 -0500

    Initial extension commit (#1)
    * Strip out a bunch of stuff to get package v0.1.0 building
    
    * Add jobArgs config parameter for launch and bump version number
    
    * Add icon and bump package version
    
    * Update icon image
    
    * Remove a few more mentions of the word "mock"
    
    * Add artificial-readme.md
    
    * Move readme.md into ms-readme.md and artificial-readme.md into readme.md
    
    * ci: create settings.yml for repository changes
    
    * Add envFile launch config variable
    
    * Update sample config in readme.md
    
    * Updated readme.md to be a bit prettier ;)
    
    * chore: updates package.json display name and description
    
    * chore: update readme language
    
    * Add copyright notices back and add Artificial copyright
    
    * Change version number to 1.0.0
    
    Co-authored-by: artificial-nick <82409344+artificial-nlynchjo@users.noreply.github.com>
    Co-authored-by: Lucas Cassiano <61432379+artificial-cassiano@users.noreply.github.com>

---
+++ src/activateWorkflowDebug.ts
@@ -0,0 +1,64 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *---------------------------------------------------------
+ * Copyright (C) Artificial, Inc. All rights reserved.
+ *---------------------------------------------------------
+ * activateWorkflowDebug.ts containes the shared extension code that can be executed both in node.js and the browser.
+ */
+
+'use strict';
+
+import * as vscode from 'vscode';
+import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
+
+export function activateWorkflowDebug(context: vscode.ExtensionContext, factory: vscode.DebugAdapterDescriptorFactory) {
+
+	// register a configuration provider for 'artificial-workflow' debug type
+	const provider = new ArtificialWorkflowDebugConfigurationProvider();
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('artificial-workflow', provider));
+
+	// register a dynamic configuration provider for 'artificial-workflow' debug type
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('artificial-workflow', {
+		provideDebugConfigurations(folder: WorkspaceFolder | undefined): ProviderResult<DebugConfiguration[]> {
+			return [
+				{
+					name: "Dynamic Launch",
+					request: "launch",
+					type: "artificial-workflow",
+					program: "${file}"
+				}
+			];
+		}
+	}, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
+
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('artificial-workflow', factory));
+}
+
+class ArtificialWorkflowDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
+
+	/**
+	 * Massage a debug configuration just before a debug session is being launched,
+	 * e.g. add all missing attributes to the debug configuration.
+	 */
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+
+		// if launch.json is missing or empty
+		if (!config.type && !config.request && !config.name) {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.document.languageId === 'python') {
+				config.type = 'artificial-workflow';
+				config.name = 'Launch';
+				config.request = 'launch';
+				config.program = '${file}';
+			}
+		}
+
+		if (!config.program) {
+			return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
+				return undefined;	// abort launch
+			});
+		}
+
+		return config;
+	}
+}
